c402854aadc6f8ed536b88148e45a0e04981b770
originalgimax@gmail.com <originalgimax@gmail.com>
2021-01-28 00:30:09 +0100

    AssemblyInfo changed

--- NetSwitch/Properties/AssemblyInfo.cs
+++ NetSwitch/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("NetSwitch")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple lagswitch tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetSwitch")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Daxter  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
